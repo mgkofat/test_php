277ecbe630921a515b8760967c6f16ff2d972df9
Phat <phat_147@hotmail.com>
2024-02-04 00:41:38 +0700

    test_php v.1

--- display_table.php
+++ display_table.php
@@ -13,8 +13,7 @@
         <link rel="stylesheet" href="style.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style_update
-.css">
+        <link rel="stylesheet" href="style_update.css">
         <title>Display Table</title>
     </head>
     <body>
