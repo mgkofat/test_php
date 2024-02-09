fc55fe1118c021aef5503f5eb4c6064f9be046a9
Phat <phat_147@hotmail.com>
2024-02-09 12:32:47 +0700

    fix bug

--- delete_table.php
+++ delete_table.php
@@ -1,31 +1,4 @@
-<?php
-    include 'include/config.php';
-    include 'include/check_session.php';
-    include 'include/check_logout.php';
-    
-if (isset($_GET['id'])) {
-    $id = mysqli_real_escape_string($conn, $_GET['id']);
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete'])) {
-        $sql = "DELETE FROM production WHERE ID = $id";
-
-        if (mysqli_query($conn, $sql)) {
-            echo "<script>alert('Data Delete Success');</script>";
-            echo "<script>window.location.href = 'display_table.php';</script>";
-        } else {
-            echo "Error: Deleting" . mysqli_error($conn);
-        }
-    }
-
-    ?>
-    <?php
-} else {
-    header("Location: display_table.php");
-    exit();
-}
-
-mysqli_close($conn);
-?><?php include 'include/function_del.php'; ?>
+<?php include 'include/function_del.php'; ?>
    <!DOCTYPE html>
     <html lang="en">
     <head>
